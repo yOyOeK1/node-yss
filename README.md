12ee477d5d60b4d7e54548d30c60e31dadf99e11
Bartlomiej Ceglik <yoyoek@wp.pl>
2025-07-08 09:37:17 -0500

    Push for 0.0.1
    All is new. It's migration from old to new node

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # node-yss
+
+Yss as a part of oiyshTerminal hosted in nodejs. So it's posible to get assets by npm.
+
+
+
+### Information
+
+What is yss? [link to old wiki ....](https://github.com/yOyOeK1/oiyshTerminal/wiki/otdm-yss)
+
+
+
+
+
+
+
+---
+
+If you see that this makes sense [ send me a ☕ ](https://ko-fi.com/B0B0DFYGS) | [Master repository](https://github.com/yOyOeK1/oiyshTerminal) | [About SvOiysh](https://www.youtube.com/@svoiysh)
---
+++ mkPrebuild.sh
@@ -0,0 +1,40 @@
+echo "---------------------"
+echo "---------------------"
+
+
+cowsay -f moose "HEllow from pre build rc !"
+
+srcPath="/OT/ySS_calibration"
+buildPath="/home/yoyo/Apps/node-yss/yss"
+
+
+echo "Doing...."
+
+echo "- clean directory in package path"
+rm -rf ${buildPath}
+
+echo "- copy current instance from [${srcPath}]"
+cp -rf ${srcPath} ${buildPath}
+
+echo "- remove all sites"
+rm -rf ${buildPath}"/sites"
+mkdir ${buildPath}"/sites"
+rm -rf ${buildPath}"/sitesTestExtDir"
+rm -rf ${buildPath}"/src"
+rm -rf ${buildPath}"/tmp/*"
+rm -rf ${buildPath}"/screenShots"
+rm -rf ${buildPath}"/yss_templates"
+rm -rf ${buildPath}"/yss1_templates"
+
+
+echo "- copy test_functions ...."
+cp -rf ${srcPath}"/sites/test_functions" \
+  ${buildPath}"/sites/"
+
+
+
+echo "-------- RC DONE"
+
+
+echo "---------------------"
+echo "---------------------"
